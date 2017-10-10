fe7566ae899bd6263e2f1beebf9a978c83e3abcb
Maxwell Benton <maxwellbenton@gmail.com>
2017-10-10 14:23:16 -0400

    Update Webpage.test.js

--- test/Webpage.test.js
+++ test/Webpage.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { expect } from 'chai';
-import { shallow } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
 import FillerText from '../src/components/FillerText';
 import Webpage from '../src/components/Webpage';
+
+Enzyme.configure({ adapter: new Adapter() })
 
 describe('<Webpage />', function () {
   const wrapper = shallow(<Webpage />);
